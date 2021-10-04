4f5066996185efdf161e7e6bfd894e44e37f14bb
elsucco <64929401+elsucco@users.noreply.github.com>
2021-10-04 18:15:30 +0200

    player page

--- src/client/html/js/app.js
+++ src/client/html/js/app.js
@@ -13,12 +13,44 @@ let app = Vue.createApp({
 
             },
             players: {
-                list: [],
+                list: [
+                    {
+                        alt: null,
+                        id: 0,
+                        name: 'Mae',
+                        ping: 0,
+                        position: { x: '', y: '', z: '' },
+                        health: '',
+                        armor: '',
+                        dimension: '',
+                        model: '',
+                        visible: '',
+                        ip: '',
+                        hwidhash: '',
+                        hwidexhash: '',
+                        socialid: ''
+                    }
+                ],
                 currentSort: 'id',
                 currentSortDir: 'asc',
                 page: '',
-                informationType: 'information',
-                currentSelected: {},
+                informationType: 'actions',
+                currentSelected: {
+                    alt: null,
+                    id: 0,
+                    name: '',
+                    ping: 0,
+                    position: { x: '', y: '', z: '' },
+                    health: 0,
+                    armor: 0,
+                    dimension: 0,
+                    model: '',
+                    visible: true,
+                    ip: '',
+                    hwidhash: '',
+                    hwidexhash: '',
+                    socialid: ''
+                },
                 listInformation: {}
             }
         }
@@ -26,10 +58,16 @@ let app = Vue.createApp({
     mounted() {
         this.show = true;
 
-        this.players.currentSelected = this.players.list[0] !== undefined ? this.players.list[0] : new this.currentSelected;
+        this.players.currentSelected = this.players.list[0] !== undefined ? this.players.list[0] : this.currentSelected;
 
         if ('alt' in window) {
-            alt.on('')
+            alt.on('data', (data) => {
+                let data1 = JSON.parse(data);
+
+                for (let i in data1) {
+                    this.addPlayerToList(data1[i].alt, data1[i].id, data1[i].name, data1[i].ping, data1[i].pos, data1[i].health, data1[i].armor, data1[i].dimension, data1[i].model, data1[i].visible, data1[i].ip, data1[i].hwidhash, data1[i].hwidexhash, data1[i].socialid);
+                }
+            })
         }
     },
     methods: {
@@ -43,10 +81,12 @@ let app = Vue.createApp({
         switchPlayerInformationType(name) {
             if (this.players.informationType !== name) this.players.informationType = name;
         },
-        addPlayerToList(name, pos, health, armor, dimension, model, visible, ip, hwidhash, hwidexhash, socialid) {
+        addPlayerToList(alt, id, name, ping, pos, health, armor, dimension, model, visible, ip, hwidhash, hwidexhash, socialid) {
             this.players.list.push({
-                _id: 0,
+                alt: alt,
+                id: id,
                 name: name,
+                ping: ping,
                 pos: pos,
                 health: health,
                 armor: armor,
@@ -58,6 +98,17 @@ let app = Vue.createApp({
                 hwidexhash: hwidexhash,
                 socialid: socialid
             })
+        },
+        playerAction(action) {
+            switch (action) {
+                case 'visible':
+                    this.players.currentSelected.visible = !this.players.currentSelected.visible;
+                    break;
+            }
+
+            if ('alt' in window) {
+                alt.emit('action', action, this.players.currentSelected, '');
+            }
         },
         sort(id) {
             this.players.currentSortDir = this.players.currentSortDir === 'asc' ? 'desc' : 'asc';
